8fcb814642a373d91812c9ced41b6d679c6c64e8
Nathan Pitman <hello@nathanpitman.com>
2015-09-28 16:53:23 +0100

    Update defines.php

--- src/Ihasco/ClientSDK/defines.php
+++ src/Ihasco/ClientSDK/defines.php
@@ -1,5 +1,5 @@
 <?php
-define('IH_API_VERSION','0.0.1');
-define('IH_API_HOST','http://ihasco.dev');
+define('IH_API_VERSION','1.0.0');
+define('IH_API_HOST','https://app.ihasco.co.uk');
 define('IH_API_ACCEPT_CONTENT','application/vnd.ihasco.v1+json');
 define('IH_API_CONNECTOR','\Ihasco\ClientSDK\Connectors\Curl');
